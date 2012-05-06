98a6e8af34ae1c74cb8d34902637e7bbb3970af7
Andrew Kelley <superjoe30@gmail.com>
2012-05-06 19:10:37 -0400

    game class

--- game.py
+++ game.py
@@ -1,27 +1,88 @@
 from __future__ import division, print_function, unicode_literals; range = xrange
+
 import pyglet
 from pygame.locals import *
+
 import sys
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+Size = namedtuple('Size', ['w', 'h'])
 
 game_title = "Dr. Chemical's Lab"
 game_fps = 60
-game_size = (1024, 600)
+game_size = Size(1024, 600)
 
-tank_loc = [(108, 54), (531, 55)]
+class Control:
+    MoveLeft = 0
+    MoveRight = 1
+    MoveUp = 2
+    MoveDown = 3
+    PullAtom = 4
+    ShootAtom = 5
+    ShootRope = 6
 
-window = pyglet.window.Window(width=game_size[0], height=game_size[1], caption=game_title)
-fps_display = pyglet.clock.ClockDisplay()
-bg = pyglet.resource.image("data/bg.png")
+class Game(object):
+    def __init__(self, window):
+        self.batch_bg = pyglet.graphics.Batch()
 
-@window.event
-def on_draw():
-    window.clear()
-    bg.blit(0, 0)
-    fps_display.draw()
+        self.img_bg = pyglet.resource.image("data/bg.png")
+        self.sprite_bg_left = pyglet.sprite.Sprite(self.img_bg, batch=self.batch_bg)
 
-def update(dt):
-    pass
+        self.window = window
+        self.window.set_handler('on_draw', self.on_draw)
+        self.window.set_handler('on_key_press', self.on_key_press)
+        self.window.set_handler('on_key_release', self.on_key_release)
 
-pyglet.clock.schedule_interval(update, 1/game_fps)
+        pyglet.clock.schedule_interval(self.update, 1/game_fps)
+        self.fps_display = pyglet.clock.ClockDisplay()
 
+        # init variables
+        self.controls = {
+            pyglet.window.key.A: Control.MoveLeft,
+            pyglet.window.key.E: Control.MoveRight,
+            pyglet.window.key.COMMA: Control.MoveUp,
+            pyglet.window.key.S: Control.MoveDown,
+        }
+        self.control_state = [False] * (len(dir(Control)) - 2)
+
+        tank_loc = [Point(108, 54), Point(531, 55)]
+        tank_dims = Size(22, 16)
+        block_size = Size(24, 24)
+        atom_colors = [
+            (70, 83, 255),
+            (357, 82, 86),
+            (139, 67, 55),
+            (36, 89, 100),
+        ]
+        man_dims = Size(1, 2)
+        man_size = Size(man_dims.w*block_size.w, man_dims.h*block_size.h)
+
+    def update(self, dt):
+        pass
+
+    def on_draw(self):
+        self.window.clear()
+        self.batch_bg.draw()
+        self.fps_display.draw()
+
+    def start(self):
+        pass
+
+    def on_key_press(self, symbol, modifiers):
+        try:
+            control = self.controls[symbol]
+            self.control_state[control] = True
+        except KeyError:
+            return
+
+    def on_key_release(self, symbol, modifiers):
+        try:
+            control = self.controls[symbol]
+            self.control_state[control] = False
+        except KeyError:
+            return
+
+window = pyglet.window.Window(width=game_size.w, height=game_size.h, caption=game_title)
+game = Game(window)
 pyglet.app.run()
