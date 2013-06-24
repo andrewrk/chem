ab1b9f0077f9ae73a3b67fd5a3d6d8619028b0dc
Andrew Kelley <superjoe30@gmail.com>
2013-06-23 23:07:47 -0400

    sound: more readable code

--- browser/chem/sound.js
+++ browser/chem/sound.js
@@ -1,29 +1,22 @@
-var _exports, Sound;
-_exports = window.Chem || (window.Chem = {});
-_exports.Sound = Sound = (function(){
-  Sound.displayName = 'Sound';
-  var audioReadyToPlay, prototype = Sound.prototype, constructor = Sound;
-  audioReadyToPlay = function(it){
-    return it.currentTime === 0 || it.currentTime === it.duration;
-  };
-  function Sound(url){
-    var this$ = this instanceof ctor$ ? this : new ctor$;
-    this$.audio_pool = [new Audio(url)];
-    return this$;
-  } function ctor$(){} ctor$.prototype = prototype;
-  prototype.play = function(){
-    var i$, ref$, len$, audio, new_audio;
-    for (i$ = 0, len$ = (ref$ = this.audio_pool).length; i$ < len$; ++i$) {
-      audio = ref$[i$];
-      if (audioReadyToPlay(audio)) {
-        audio.play();
-        return audio;
-      }
+var _exports = window.Chem || (window.Chem = {});
+_exports.Sound = Sound;
+
+function Sound(url) {
+  this.audio_pool = [new Audio(url)];
+}
+
+function audioReadyToPlay(audio) {
+  return audio.currentTime === 0 || audio.currentTime === audio.duration;
+}
+
+Sound.prototype.play = function() {
+  for (var i = 0; i < this.audio_pool.length; ++i) {
+    if (audioReadyToPlay(audio)) {
+      audio.play();
+      return audio;
     }
-    new_audio = new Audio(this.audio_pool[0].currentSrc);
-    new_audio.play();
-    this.audio_pool.push(new_audio);
-    return new_audio;
-  };
-  return Sound;
-}());
+  }
+  var new_audio = new Audio(this.audio_pool[0].currentSrc);
+  new_audio.play();
+  this.audio_pool.push(new_audio);
+};
