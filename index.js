253595770af02bd680d365bd80773c04f48626f4
Andrew Kelley <superjoe30@gmail.com>
2013-07-22 06:01:10 -0400

    rewrite resource loading framework
     * removed chem.onReady(fn)
     * resources.onReady(fn) moved to resources.on('ready', fn)
     * resources is a normal EventEmitter just like everything else
     * added resources.on('progress', fn)
     * resources.bootstrap() no longer called automatically
     * added resources.getTextFile(name, cb)
     * added engine.showLoadProgressBar()

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ var resources = require('./lib/resources');
 
 module.exports = {
   vec2d: require('vec2d'),
-  Engine: require('./lib/engine'),
   button: require('./lib/button'),
+  resources: resources,
+
+  Engine: require('./lib/engine'),
   Sound: require('./lib/sound'),
   Sprite: require('./lib/sprite'),
   Label: require('./lib/label'),
   Batch: require('./lib/batch'),
-  resources: resources,
-  onReady: resources.onReady,
 };
-
-resources.bootstrap();
